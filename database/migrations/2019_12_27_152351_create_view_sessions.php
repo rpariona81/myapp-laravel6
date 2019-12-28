3800d0429fb9acc532dc378341812d1c13d23288
rpariona81 <ronald.pariona.isidro@gmail.com>
2019-12-28 00:20:18 -0500

    v_sessions version mysql

--- database/migrations/2019_12_27_152351_create_view_sessions.php
+++ database/migrations/2019_12_27_152351_create_view_sessions.php
@@ -14,10 +14,28 @@ class CreateViewSessions extends Migration
     public function up()
     {
         if (config('database.default') == 'mysql') {
-            DB::unprepared("");
+            DB::unprepared("
+            CREATE VIEW v_sessions AS
+            SELECT
+            last_activity,
+            FROM_UNIXTIME(t1.last_activity) as last_activity_datetime,
+            FROM_UNIXTIME(t1.last_activity, '%Y-%m-%d') as last_activity_date,
+            FROM_UNIXTIME(t1.last_activity-DATEDIFF(NOW(),FROM_UNIXTIME(t1.last_activity))) as session_datetime_local,
+            FROM_UNIXTIME(t1.last_activity-DATEDIFF(NOW(),FROM_UNIXTIME(t1.last_activity)), '%Y-%m-%d') as session_date_local,
+            t1.ip_address,
+            t2.id as user_id,
+            t2.username, t2.email,
+            t3.role_id,
+            t4.rolename
+            FROM t_sessions t1
+            LEFT JOIN t_users t2 ON t1.user_id=t2.id
+            LEFT JOIN t_role_user t3 ON t2.id=t3.user_id
+            LEFT JOIN t_roles t4 ON t3.role_id=t4.id
+            ");
         }elseif((config('database.default') == 'sqlsrv')) {
             DB::unprepared("
-            CREATE VIEW v_sessions AS SELECT 
+            CREATE VIEW v_sessions AS
+            SELECT
             last_activity,
             DATEADD(s, t1.last_activity , '19700101') as last_activity_datetime,
             CONVERT(date,DATEADD(s, t1.last_activity , '19700101'),106) as last_activity_date,
@@ -27,11 +45,11 @@ class CreateViewSessions extends Migration
             t2.id as user_id,
             t2.username, t2.email,
             t3.role_id,
-            t4.rolename   
-            FROM t_sessions t1 
-            LEFT JOIN t_users t2 ON t1.user_id=t2.id 
-            LEFT JOIN t_role_user t3 ON t2.id=t3.user_id 
-            LEFT JOIN t_roles t4 ON t3.role_id=t4.id 
+            t4.rolename
+            FROM t_sessions t1
+            LEFT JOIN t_users t2 ON t1.user_id=t2.id
+            LEFT JOIN t_role_user t3 ON t2.id=t3.user_id
+            LEFT JOIN t_roles t4 ON t3.role_id=t4.id
             WHERE CONVERT(date,DATEADD(s, t1.last_activity , '19700101'),106) = CONVERT(date,GETDATE(),106)
             ");
         }
@@ -44,6 +62,6 @@ class CreateViewSessions extends Migration
      */
     public function down()
     {
-        DB::unprepared("DROP VIEW v_sessions");
+        DB::unprepared("DROP VIEW IF EXISTS v_sessions");
     }
 }
