7cfddb9a8b9e6013bc31ada46a63d7372b8cab3f
rpariona81 <ronald.pariona.isidro@gmail.com>
2023-06-11 02:36:59 -0500

    table user lista

--- database/migrations/2019_12_26_191644_create_table_role_user.php
+++ database/migrations/2019_12_26_191644_create_table_role_user.php
@@ -14,7 +14,6 @@ class CreateTableRoleUser extends Migration
     public function up()
     {
         Schema::create('t_role_user', function (Blueprint $table) {
-<<<<<<< HEAD
             $table->bigIncrements('id');
             //$table->biginteger('role_id')->unsigned();
             //$table->biginteger('user_id')->unsigned();
@@ -24,14 +23,12 @@ class CreateTableRoleUser extends Migration
             
             $table->foreign('user_id')->references('id')->on('t_users');
             $table->timestamps();
-=======
             /*$table->bigIncrements('id');
             $table->biginteger('role_id')->unsigned();
             $table->biginteger('user_id')->unsigned();*/
             $table->increments('id');
             $table->integer('user_id')->unsigned();  //Primero define usuario
             $table->integer('role_id')->unsigned();  //Luego rol
->>>>>>> 754d150d2b279b46aa6e9a981ca54e36aae82791
         });
     }
 
